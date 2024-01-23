9197a018d40313cf271f80a140634966869c86d7
MiyukiNishimori2023 <mgxxx32123@gmail.com>
2024-01-23 19:21:58 +0900

    スタートボタンがアニメーションするように

--- Assets/Scenes/Title/Scripts/TitleUIManager.cs
+++ Assets/Scenes/Title/Scripts/TitleUIManager.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class TitleUIManager : MonoBehaviour
 {
     [SerializeField]
     private Button _startButton;
 
+    private Tween _startButtonTween;
+
     public void Init()
     {
         _startButton.onClick.AddListener(OnClickStartButton);
+        _startButtonTween = _startButton.transform
+            .DOScale(1.2f, 0.4f)
+            .SetLoops(-1, LoopType.Yoyo);
     }
 
     private void OnClickStartButton()
     {
+        _startButtonTween.Kill();
         MainSystem.Instance.AppSceneManager.ChangeScene(ConstSceneName.Game, fadeType: FadeType.ColorWhite);
     }
 }
