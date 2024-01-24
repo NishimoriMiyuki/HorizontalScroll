bd6f927b729c9a8f97fca0f46d57372e78e63ebe
MiyukiNishimori <mgxxx32123@gmail.com>
2024-01-24 17:01:03 +0900

    不要な画像削除、画像追加

--- Assets/Scripts/Const/ConstAssetAddress.cs
+++ Assets/Scripts/Const/ConstAssetAddress.cs
@@ -1,6 +1,6 @@
 public class ConstAssetAddress
 {
     public const string WorkIcon = "Assets/Scenes/Game/Sprites/pc";
-    public const string FeelDisabledIcon = "Assets/Scenes/Game/Sprites/symbol[symbol_2]";
+    public const string FeelDisabledIcon = "Assets/Scenes/Game/Sprites/bikkuri_01.png";
     public const string MonitorIcon = "Assets/Scenes/Game/Sprites/eye12_side2";
 }
