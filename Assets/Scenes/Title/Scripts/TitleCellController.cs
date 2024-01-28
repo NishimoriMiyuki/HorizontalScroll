d21ff066a297601ddc2aa5d3f174d22ec9b77c32
MiyukiNishimori2023 <mgxxx32123@gmail.com>
2024-01-28 16:22:23 +0900

    タイトルシーンの素材追加、画像差し替え

--- Assets/Scenes/Title/Scripts/TitleCellController.cs
+++ Assets/Scenes/Title/Scripts/TitleCellController.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class TitleCellController : MonoBehaviour
 {
     [SerializeField]
-    private Image _image;
+    private Image _titleImage, _possessionImage;
 
     public async UniTask Init(Title titleData)
     {
-        _image.sprite = await MainSystem.Instance.AddressableManager.LoadAssetAsync<Sprite>(titleData.name_address);
+        _titleImage.sprite = await MainSystem.Instance.AddressableManager.LoadAssetAsync<Sprite>(titleData.name_address);
+        _possessionImage.gameObject.SetActive(true);
 
         if (!MainSystem.Instance.PlayerData.titles.Any(t => t.title_id == titleData.id))
         {
-            _image.color = Color.gray;
+            _titleImage.color = Color.gray;
+            _possessionImage.gameObject.SetActive(false);
         }
     }
 }
